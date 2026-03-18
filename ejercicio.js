f8342936192d5afd4de0006f30e2a0f50e6451cb
emotIQ14 <anderbilbaocastejon@gmail.com>
2026-03-18 09:14:12 +0100

    Add JavaScript exercise solution

---
+++ ejercicio.js
@@ -0,0 +1,9 @@
+function comprobarNumero(a, b, c, d) {
+  const resultado = (a + b) * (c + d);
+
+  if (resultado > 50) {
+    console.log("¡El número es mayor que 50!");
+  } else if (resultado < 50) {
+    console.log("¡El número es menor que 50!");
+  }
+}
